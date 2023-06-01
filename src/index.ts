8c8819ff41dffd9780becf998daef2e76ce18a35
yousuf4you <yousuf360.ya@gmail.com>
2023-06-01 17:39:21 +0600

    almost a result

---
+++ src/index.ts
@@ -0,0 +1,31 @@
+import "colors";
+
+export default class Log {
+  static getTime() {
+    const t = new Date();
+    const hour = t.getHours();
+    const minute = t.getMinutes();
+    const second = t.getSeconds();
+    const m = hour >= 12 ? "PM" : "AM";
+
+    return `[${hour}:${minute}:${second} ${m}] `.dim;
+  }
+
+  static success(message: string) {
+    console.log(this.getTime() + "🚀 " + message + " 🚀".green);
+  }
+  static info(message: string) {
+    console.log(this.getTime() + "ℹ️ " + message.bgCyan.white + " ℹ️");
+  }
+  static warn(message: string) {
+    console.log(this.getTime() + "⚠️ " + message.underline.yellow + " ⚠️");
+  }
+  static danger(message: string) {
+    console.log(this.getTime() + "🔴 " + message.red + " 🔴");
+  }
+}
+
+Log.success("Hello World");
+Log.info("Hello World");
+Log.warn("Hello World");
+Log.danger("Hello World");
